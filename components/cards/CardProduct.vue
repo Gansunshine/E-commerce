3085b610905e56c0819316de3eb055bf6aaec9b8
Gansunshine <agansaja22@gmail.com>
2023-10-27 14:34:52 +0700

    pertemuan 4

--- components/cards/CardProduct.vue
+++ components/cards/CardProduct.vue
@@ -1,33 +1,52 @@
 <script lang="ts" setup>
-const props = defineProps({
+    import type { Products } from '~/types/products';
+    const props = defineProps({
     product: {
-        type: Object,
-        default: {},
+    type: Object,
+    default: {},
     }
-});
+    });
+    const oneProduct = ref(props.product);
+    const addCart = () => {
+    oneProduct.value.isCart = !oneProduct.value.isCart
+    let localStorageData = localStorage.getItem("products");
+    let productOfCart: Products[] = [];
+    if (localStorageData) {
+    productOfCart = JSON.parse(localStorageData);
+    }
+    if (oneProduct.value.isCart) {
+    productOfCart.push(oneProduct.value);
+    localStorage.setItem("products", JSON.stringify(productOfCart));
+    } else {
+    productOfCart = productOfCart.filter((item) => item.id !==
+    oneProduct.value.id);
+    localStorage.setItem("products", JSON.stringify(productOfCart));
+    }
+    }
 </script>
 
 <template>
     <section class="bg-white shadow-xl rounded-xl overflow-hidden">
         <div :class="`w-full h-[200px] p-5 bg-gray-300`">
-            <img :src="props.product.image" class="w-full h-full object-contain"/>
-            </div>
-            <div class="px-5 pb-5 pt-9 relative">
-
-            <NuxtLink :to="`/product/${props.product.id}`" ><h3 class="text-lg font-
-            bold mb-4 text-limit limit-2">{{ props.product.name }}</h3></NuxtLink>
-
+            <img :src="oneProduct.image" class="w-full h-full object-contain"/>
+        </div>
+        <div class="px-5 pb-5 pt-9 relative">
+            
+            <NuxtLink :to="`/product/${oneProduct.id}`" ><h3 class="text-lg font-
+            bold mb-4 text-limit limit-2">{{ oneProduct.name }}</h3></NuxtLink>
+            
             <div class="flex justify-between items-center">
-            <span class="text-sm font-normal">{{ props.product.category }}</span>
-            <span class="text-sm font-normal">${{ props.product.price }}</span>
+            <span class="text-sm font-normal">{{ oneProduct.category }}</span>
+            <span class="text-sm font-normal">${{ oneProduct.price }}</span>
             </div>
+            <div :class="`cursor-pointer absolute -top-5 right-7 w-[50px] h-[50px]
+${oneProduct.isCart ? 'bg-blue-600 text-white' : 'bg-white'} shadow-xl
 
-            <div :class="`cursor-pointer absolute -top-5 right-7 w-[50px] h-[50px] bg-
-            white shadow-xl rounded-full flex justify-center items-center hover:bg-blue-600
+rounded-full flex justify-center items-center hover:bg-blue-600 hover:text-
+white transition duration-300`" @click="addCart">
 
-            hover:text-white transition duration-300`">
-            <i class="ri-shopping-cart-2-line"></i>
-            </div>
+<i class="ri-shopping-cart-2-line"></i>
+</div>
         </div>
     </section>
 </template>
--- pages/product/index.vue
+++ pages/product/index.vue
@@ -9,18 +9,19 @@ return products;
 });
 </script>
 <template>
-<section>
-<div class="container">
-<div class="py-10">
-<div class="mb-6 flex justify-end gap-6">
-<Dropdown @selected-category="selectedCategory = $event" />
-</div>
-<div class="flex gap-6 flex-wrap mx-auto">
-<template v-for="(item, index) in allProducts" :key="index">
-<CardsCardProduct :product="item" class="w-[calc(100%/4-18px)]"/>
-</template>
-</div>
-</div>
-</div>
-</section>
+    <section>
+        <div class="container">
+            <div class="py-10">
+                <div class="mb-6 flex justify-end gap-6">
+                    <NuxtLink to="/category/create" class="bg-orange-500 text-white flex justify-center items-center px-3 rounded-lg">Create Category</NuxtLink>
+                    <Dropdown @selected-category="selectedCategory = $event" />
+                </div>
+                <div class="flex gap-6 flex-wrap mx-auto">
+                    <template v-for="(item, index) in allProducts" :key="index">
+                        <CardsCardProduct :product="item" class="w-[calc(100%/4-18px)]"/>
+                    </template>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
